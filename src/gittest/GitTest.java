e3164297c8fa8c90ff6d89d782afd450e36bec91
tsnodgrass96 <tsnodgrass96@gmail.com>
2015-09-17 11:36:09 -0500

    This is where I would put my commit comment.... IF I HAD ONE!!!! [insert photo of Timmy's dad]

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, world!");
+        System.out.println("Hi, I'm Doug Dimmadome owner of the Dimesdale Dimmadome");
     }
     
 }
